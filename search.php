2f29cd1f0b20ee91b6f7a1f79b5e52b937d426c1
CrunchDrop <daulton@zoho.com>
2018-02-22 22:28:10 -0700

    Update and rename search.php3 to search.php

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?
-include("lib.php3");
-include("setup.php3"); 
+include("lib.php");
+include("setup.php");
 
 if ($days=="") {
    $days = $default_days;
@@ -14,11 +14,11 @@ include("short_header.inc");
 <!--
 function ReadMsg (id,pid,days,js,lang) {
     if ( window.name != "newmsg" && (<?echo intval($view_new_win)?> == 1) ) {
-        w = window.open ("readmsg.php3?id=" + id + "&pid=" + pid +"&days="+days+"&js="+js+"&lang="+lang, "newmsg", "<? echo $js_window_params ?>");
+        w = window.open ("readmsg.php?id=" + id + "&pid=" + pid +"&days="+days+"&js="+js+"&lang="+lang, "newmsg", "<? echo $js_window_params ?>");
         w.focus();
     }
     else {
-        return 'readmsg.php3?id=' + id + '&pid=' + pid +'&days=' +days +'&js='+js+'&lang='+lang;
+        return 'readmsg.php?id=' + id + '&pid=' + pid +'&days=' +days +'&js='+js+'&lang='+lang;
     }
     
     return '#';
@@ -30,15 +30,15 @@ function ReadMsg (id,pid,days,js,lang) {
 <?include("srch.inc");?>
 <p>
 
-<A href="./index.php3?days=<? echo $days; ?>&js=<? echo $js; ?>&lang=<? echo $lang; ?>" class=t <?mouse_text($msg["all_articles"])?>><?echo $msg["all_articles"]?></A>
+<A href="./index.php?days=<? echo $days; ?>&js=<? echo $js; ?>&lang=<? echo $lang; ?>" class=t <?mouse_text($msg["all_articles"])?>><?echo $msg["all_articles"]?></A>
 
 <HR>
 <?
-$args = split(" ", $what);
+$args = preg_split(" ", $what);
 ## $rule : "AND" or "OR" or "EXACT"
 if ( !empty($what) && count($args) > 0 ) {
-    mysql_pconnect("$mysql_host","$mysql_user","$mysql_password");
-    mysql_select_db("$mysql_base");
+    mysqli_connect("$mysql_host","$mysql_user","$mysql_password");
+    mysqli_select_db("$mysql_base");
 
     if ($rule != "EXACT") {
         for($i=0; $i<count($args); $i++) {
@@ -61,24 +61,24 @@ if ( !empty($what) && count($args) > 0 ) {
                  "         (content LIKE '%$what%') ) ";
     }
 
-    $res = mysql_query("select distinct id,pid,subj,author,email,date_format(times, '%d/%m/%Y %H:%i') as ttimes ".
+    $res = mysqli_query("select distinct id,pid,subj,author,email,date_format(times, '%d/%m/%Y %H:%i') as ttimes ".
                        "from $mysql_table where $cond order by times desc" );
 
-    $num = mysql_num_rows($res);
+    $num = mysqli_num_rows($res);
     if ($num > 0) {
         print $msg["tot_found"] . ": <B>$num</B> <P>";
 
-?>        
+?>
 <table align="left" border="0" cellpadding="2" cellspacing="0" bgcolor="<? echo $titlecolor; ?>"><TR><TD><table border=0 cellspacing="0" cellpadding="3">
 
-<?  
-        while ( $d = mysql_fetch_array($res) ) {
-            $id  = $d["id"]; 
+<?
+        while ( $d = mysqli_fetch_array($res) ) {
+            $id  = $d["id"];
             $pid = $d["pid"];
             
             print "<TR valign=center bgcolor=" . RCount() . " height=11>";
             print "<td>".
-            "<A class=s HREF=\"readmsg.php3?id=$id&pid=$pid&days=10000&js=$js&lang=$lang\" OnClick=\"window.status=''; this.href = ReadMsg($id,$pid,100000,$js,'$lang');\" ";
+            "<A class=s HREF=\"readmsg.php?id=$id&pid=$pid&days=10000&js=$js&lang=$lang\" OnClick=\"window.status=''; this.href = ReadMsg($id,$pid,100000,$js,'$lang');\" ";
             mouse_text($msg['view_article']);
             echo " >$d[subj]</a> </td><td class=t> " ;
             echo "$d[author]</td><td class=d>$d[ttimes]</td></tr>\n";
