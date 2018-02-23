7ddc4e2d42f9edbccf8c3ddf24a4affac569646d
CrunchDrop <daulton@zoho.com>
2018-02-22 22:14:53 -0700

    Update and rename index.php3 to index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-include("setup.php3");
+include("setup.php");
 
 //Create forum mysql_table if it not exists
-mysql_pconnect("$mysql_host","$mysql_user","$mysql_password");
-$res = mysql_select_db("$mysql_base");
+mysqli_connect("$mysql_host","$mysql_user","$mysql_password");
+$res = mysqli_select_db("$mysql_base");
 if (!$res)
 {
     echo "<html><body><h1>MySQL Database $mysql_base not created.</h1></body></html>";
     exit;
 }
 
-$res = @mysql_query("DESC $mysql_table");
-if ( mysql_errno() != 0 )   //$mysql_table autocreating
+$res = @mysqli_query("DESC $mysql_table");
+if ( mysqli_errno() != 0 )   //$mysql_table autocreating
 {
-    mysql_query("CREATE TABLE $mysql_table (
+    mysqli_query("CREATE TABLE $mysql_table (
         id int(11) DEFAULT '0' NOT NULL auto_increment,
         pid int(11),
         times datetime,
@@ -54,7 +54,7 @@ function NewMsg (days,lang) {
     if (window.name == winname) {
         winname = "new1";
     }
-    w = window.open ("newmsg.php3?id=0&days=" + days + "&lang=" + lang, winname, "<? echo $js_window_params ?>");
+    w = window.open ("newmsg.php?id=0&days=" + days + "&lang=" + lang, winname, "<? echo $js_window_params ?>");
     w.focus();
     return "#";
 }
@@ -62,7 +62,7 @@ function NewMsg (days,lang) {
 <?
 if(!isset($js))
 {
-    echo "window.location=\"index.php3?js=1&days=$days&lang=$lang\"";
+    echo "window.location=\"index.php?js=1&days=$days&lang=$lang\"";
     $js=0; //javascript test
 }
 ?>
@@ -76,13 +76,13 @@ if (!empty($with_search)){
 }
 ?>
 
-<p><a href="newmsg.php3?id=0&days=<? echo $days; ?>&lang=<? echo $lang; ?>" OnClick="this.href=NewMsg(<?echo "$days,'$lang'"?>);" <?mouse_text($msg["new_thread"])?>><?echo $msg["new_thread"]?></a> 
+<p><a href="newmsg.php?id=0&days=<? echo $days; ?>&lang=<? echo $lang; ?>" OnClick="this.href=NewMsg(<?echo "$days,'$lang'"?>);" <?mouse_text($msg["new_thread"])?>><?echo $msg["new_thread"]?></a>
 <br><br>
 
 <?
 $ppid = 0;
 $view_articles_for_last=1;
-include("forum.php3");
+include("forum.php");
 
 do_stats(0);
 
