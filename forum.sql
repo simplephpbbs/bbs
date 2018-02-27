ecf578410742a21d3fdd7a3fc3da3bfd3a21535c
CrunchDrop <daulton@zoho.com>
2018-02-26 19:34:16 -0700

    Update forum.sql

--- forum.sql
+++ forum.sql
@@ -1,14 +1,14 @@
 CREATE TABLE forum (
-  id int(11) DEFAULT '0' NOT NULL auto_increment,
-  pid int(11),
-  times datetime,
-  subj varchar(128),
-  author varchar(50),
-  email varchar(50),
-  content text,
-  archive enum('Y','N'),
-  level int(11),
-  parent enum('Y','N'),
-  PRIMARY KEY (id)
+  id int( 11 ) NOT NULL auto_increment ,
+  pid int( 11 ) ,
+  times datetime ,
+  subj varchar( 128 ) ,
+  author varchar( 50 ) ,
+  email varchar( 50 ) ,
+  content text ,
+  archive enum( 'Y','N' ) ,
+  level int( 11 ) ,
+  parent enum( 'Y','N' ) ,
+  PRIMARY KEY ( id )
 );
 
